528e79642d78bc00e628bc7832df0b4a6f59e04e
Juan Pablo Uriol Balbín <jpuriol@gmail.com>
2023-12-02 17:52:53 +0100

    Day 2. Part 2

--- src/Day02.kt
+++ src/Day02.kt
@@ -52,15 +52,49 @@ fun main() {
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        var res = 0
+
+        for (game in input) {
+
+            val id = extractGameNumber(game) ?: error("unable to extract game number: $game")
+
+            var maxRed = 0
+            var maxGreen = 0
+            var maxBlue = 0
+
+            val draws = game.removePrefix("Game $id:").split(";")
+            for (draw in draws) {
+                val balls = draw.split(',')
+                val colors = balls.map { ball ->
+                    val n = extractNumber(ball) ?: error("unable to extract ball number: $ball")
+                    val color = ball.removePrefix(" $n ")
+                    Pair(n, color)
+                }
+
+                for ((n, color) in colors) {
+                    when (color) {
+                        "red" -> if (n > maxRed) maxRed = n
+                        "green" -> if (n > maxGreen) maxGreen = n
+                        "blue" -> if (n > maxBlue) maxBlue = n
+                        else -> error("unknown color: $color")
+                    }
+                }
+            }
+
+            res += (maxRed * maxGreen * maxBlue)
+        }
+
+
+
+        return res
     }
 
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day02_test")
-    check(part1(testInput) == 8)
+    check(part2(testInput) == 2286)
 
     val input = readInput("Day02")
     part1(input).println()
-    // part2(input).println()
+    part2(input).println()
 }
