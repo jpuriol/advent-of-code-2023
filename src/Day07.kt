7172dcceed30d20efc5c640d692d2d37dec615e1
Juan Pablo Uriol Balbín <jpuriol.etraid@grupoetra.com>
2023-12-07 12:52:16 +0100

    Day 07. Part 2. With test data

--- src/Day07.kt
+++ src/Day07.kt
@@ -2,7 +2,7 @@ fun main() {
 
     data class Hand(val cards: List<Card>, val bid: Int)
 
-    fun Hand(input: String) : Hand {
+    fun Hand(input: String): Hand {
         val (cardsStr, bidStr) = input.split(" ")
 
         return Hand(
@@ -16,12 +16,12 @@ fun main() {
         val hands = input.map { Hand(it) }
 
         val winnings = hands.sortedWith(
-            compareBy<Hand> {HandType(it.cards).strength}
-                .thenBy { it.cards[0].strength }
-                .thenBy { it.cards[1].strength }
-                .thenBy { it.cards[2].strength }
-                .thenBy { it.cards[3].strength }
-                .thenBy { it.cards[4].strength }
+            compareBy<Hand> { HandType(it.cards).strength }
+                .thenBy { it.cards[0].strengthP1 }
+                .thenBy { it.cards[1].strengthP1 }
+                .thenBy { it.cards[2].strengthP1 }
+                .thenBy { it.cards[3].strengthP1 }
+                .thenBy { it.cards[4].strengthP1 }
         ).mapIndexed { index, hand ->
             val rank = index + 1
             rank * hand.bid
@@ -32,38 +32,53 @@ fun main() {
     }
 
 
-    fun part2(input: List<String>): Int {
-        return input.size
+    fun part2(input: List<String>): Long {
+
+        val hands = input.map { Hand(it) }
+
+        val winnings = hands.sortedWith(
+            compareBy<Hand> { HandType(it.cards, Part.TWO).strength }
+                .thenBy { it.cards[0].strengthP2 }
+                .thenBy { it.cards[1].strengthP2 }
+                .thenBy { it.cards[2].strengthP2 }
+                .thenBy { it.cards[3].strengthP2 }
+                .thenBy { it.cards[4].strengthP2 }
+        ).mapIndexed { index, hand ->
+            val rank = index + 1
+            rank.toLong() * hand.bid.toLong()
+        }.sum()
+
+        return winnings
     }
 
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day07_test")
-    check(part1(testInput) == 6440)
+    check(part2(testInput) == 5905L)
 
     val input = readInput("Day07")
     part1(input).println()
     part2(input).println()
 }
 
-enum class Card(val strength: Int) {
-    A(13),
-    K(12),
-    Q(11),
-    J(10),
-    T(9),
-    NINE(8),
-    EIGHTH(7),
-    SEVEN(6),
-    SIX(5),
-    FIVE(4),
-    FOUR(3),
-    THREE(2),
-    TWO(1),
+enum class Card(val strengthP1: Int, val strengthP2: Int) {
+    A(strengthP1 = 13, strengthP2 = 13),
+    K(strengthP1 = 12, strengthP2 = 12),
+    Q(strengthP1 = 11, strengthP2 = 11),
+    J(strengthP1 = 10, strengthP2 = -1), // The joker on part2 has the lowest strength
+    T(strengthP1 = 9, strengthP2 = 9),
+    NINE(strengthP1 = 8, strengthP2 = 8),
+    EIGHTH(strengthP1 = 7, strengthP2 = 7),
+    SEVEN(strengthP1 = 6, strengthP2 = 6),
+    SIX(strengthP1 = 5, strengthP2 = 5),
+    FIVE(strengthP1 = 4, strengthP2 = 4),
+    FOUR(strengthP1 = 3, strengthP2 = 3),
+    THREE(strengthP1 = 2, strengthP2 = 2),
+    TWO(strengthP1 = 1, strengthP2 = 1),
 
 }
 
-fun Card(char: Char) : Card {
+fun Card(char: Char): Card {
     return when (char) {
         'A' -> Card.A
         'K' -> Card.K
@@ -92,16 +107,90 @@ enum class HandType(val strength: Int) {
     HIGH_CARD(1)
 }
 
-fun HandType(hand: List<Card>) : HandType {
+enum class Part {
+    ONE,
+    TWO
+}
+
+fun HandType(hand: List<Card>, part: Part = Part.ONE): HandType {
     val counts = hand.groupingBy { it }.eachCount()
 
-    return when {
-        counts.values.contains(5) -> HandType.FIVE_OF_A_KIND
-        counts.values.contains(4) -> HandType.FOUR_OF_A_KIND
-        counts.values.contains(3) && counts.values.contains(2) -> HandType.FULL_HOUSE
-        counts.values.contains(3) -> HandType.THREE_OF_A_KIND
-        counts.values.count { it == 2 } == 2 -> HandType.TWO_PAIR
-        counts.values.contains(2) -> HandType.ONE_PAIR
-        else -> HandType.HIGH_CARD
+    return when (part) {
+        Part.ONE -> {
+            when {
+                counts.values.contains(5) -> HandType.FIVE_OF_A_KIND
+                counts.values.contains(4) -> HandType.FOUR_OF_A_KIND
+                counts.values.contains(3) && counts.values.contains(2) -> HandType.FULL_HOUSE
+                counts.values.contains(3) -> HandType.THREE_OF_A_KIND
+                counts.values.count { it == 2 } == 2 -> HandType.TWO_PAIR
+                counts.values.contains(2) -> HandType.ONE_PAIR
+                else -> HandType.HIGH_CARD
+            }
+        }
+
+        Part.TWO -> {
+            val jokers = counts[Card.J] ?: 0
+            val newCounts = counts.filter { it.key != Card.J }
+
+            val possibleType = when {
+                newCounts.values.contains(5) -> HandType.FIVE_OF_A_KIND
+                newCounts.values.contains(4) -> HandType.FOUR_OF_A_KIND
+                newCounts.values.contains(3) && newCounts.values.contains(2) -> HandType.FULL_HOUSE
+                newCounts.values.contains(3) -> HandType.THREE_OF_A_KIND
+                newCounts.values.count { it == 2 } == 2 -> HandType.TWO_PAIR
+                newCounts.values.contains(2) -> HandType.ONE_PAIR
+                else -> HandType.HIGH_CARD
+            }
+
+            when(jokers) {
+                0 -> possibleType
+                1 -> when(possibleType) {
+                    HandType.FIVE_OF_A_KIND -> error("Impossible to have 5 of a kind with a joker")
+                    HandType.FOUR_OF_A_KIND -> HandType.FIVE_OF_A_KIND
+                    HandType.FULL_HOUSE -> HandType.FOUR_OF_A_KIND
+                    HandType.THREE_OF_A_KIND -> HandType.FOUR_OF_A_KIND
+                    HandType.TWO_PAIR -> HandType.FULL_HOUSE
+                    HandType.ONE_PAIR -> HandType.THREE_OF_A_KIND
+                    HandType.HIGH_CARD -> HandType.ONE_PAIR
+                }
+                2 -> when(possibleType) {
+                    HandType.FIVE_OF_A_KIND -> error("Impossible to have 5 of a kind with a joker")
+                    HandType.FOUR_OF_A_KIND -> error("Impossible to have 4 of a kind with 2 jokers")
+                    HandType.FULL_HOUSE -> HandType.FIVE_OF_A_KIND
+                    HandType.THREE_OF_A_KIND -> HandType.FIVE_OF_A_KIND
+                    HandType.TWO_PAIR -> HandType.FULL_HOUSE
+                    HandType.ONE_PAIR -> HandType.FOUR_OF_A_KIND
+                    HandType.HIGH_CARD -> HandType.TWO_PAIR
+                }
+                3 -> when(possibleType) {
+                    HandType.FIVE_OF_A_KIND -> error("Impossible to have 5 of a kind with a joker")
+                    HandType.FOUR_OF_A_KIND -> error("Impossible to have 4 of a kind with 3 jokers")
+                    HandType.FULL_HOUSE -> error("Impossible to have a full house with 3 jokers")
+                    HandType.THREE_OF_A_KIND -> error("Impossible to have 3 of a kind with 3 jokers")
+                    HandType.TWO_PAIR -> error("Impossible to have 2 pairs with 3 jokers")
+                    HandType.ONE_PAIR -> HandType.FIVE_OF_A_KIND
+                    HandType.HIGH_CARD -> HandType.FOUR_OF_A_KIND
+                }
+                4 -> when(possibleType) {
+                    HandType.FIVE_OF_A_KIND -> error("Impossible to have 5 of a kind with a joker")
+                    HandType.FOUR_OF_A_KIND -> error("Impossible to have 4 of a kind with 4 jokers")
+                    HandType.FULL_HOUSE -> error("Impossible to have a full house with 4 jokers")
+                    HandType.THREE_OF_A_KIND -> error("Impossible to have 3 of a kind with 4 jokers")
+                    HandType.TWO_PAIR -> error("Impossible to have 2 pairs with 4 jokers")
+                    HandType.ONE_PAIR -> error("Impossible to have 1 pair with 4 jokers")
+                    HandType.HIGH_CARD -> HandType.FIVE_OF_A_KIND
+                }
+                5 -> when(possibleType) {
+                    HandType.FIVE_OF_A_KIND -> error("Impossible to have 5 of a kind with a joker")
+                    HandType.FOUR_OF_A_KIND -> error("Impossible to have 4 of a kind with 5 jokers")
+                    HandType.FULL_HOUSE -> error("Impossible to have a full house with 5 jokers")
+                    HandType.THREE_OF_A_KIND -> error("Impossible to have 3 of a kind with 5 jokers")
+                    HandType.TWO_PAIR -> error("Impossible to have 2 pairs with 5 jokers")
+                    HandType.ONE_PAIR -> error("Impossible to have 1 pair with 5 jokers")
+                    HandType.HIGH_CARD -> HandType.FIVE_OF_A_KIND
+                }
+                else -> error("Invalid number of jokers $jokers")
+            }
+        }
     }
 }
