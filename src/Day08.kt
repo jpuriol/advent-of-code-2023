acf6b8baa45567a8e34e9370e2adf0d71c30b122
Juan Pablo Uriol Balbín <jpuriol@gmail.com>
2023-12-11 10:15:18 +0100

    Day 8. Part 2

--- src/Day08.kt
+++ src/Day08.kt
@@ -1,6 +1,6 @@
 fun main() {
 
-    fun part1(input: List<String>): Int {
+    fun parse(input: List<String>): Pair<List<Instruction>, Map<String, Node>> {
         val instructions = input[0].map { c ->
             when (c) {
                 'L' -> Instruction.LEFT
@@ -17,6 +17,12 @@ fun main() {
                 right = right.trim().removeSuffix(")")
             )
         }.associateBy { it.name }
+
+        return Pair(instructions, nodes)
+    }
+
+    fun part1(input: List<String>): Int {
+        val (instructions, nodes) = parse(input)
 
         var currentNode = nodes["AAA"] ?: error("No node with name 'AAA' found")
         var currentInstruction = 0
@@ -39,14 +45,47 @@ fun main() {
     }
 
 
-    fun part2(input: List<String>): Int {
-        return input.size
+    fun part2(input: List<String>): Long {
+
+        val (instructions, nodes) = parse(input)
+
+        val startNodes = nodes.filter { it.key.endsWith('A') }.map { it.value }
+
+        val stepsToFinalNode = startNodes.associateWith { node ->
+            var currentNode = node
+
+            var currentInstruction = 0
+            var steps = 0L
+
+            while (!currentNode.name.endsWith('Z')) {
+                val instruction = instructions[currentInstruction]
+
+                currentNode = when (instruction) {
+                    Instruction.LEFT -> nodes[currentNode.left]
+                        ?: error("No node with name '${currentNode.left}' found")
+
+                    Instruction.RIGHT -> nodes[currentNode.right]
+                        ?: error("No node with name '${currentNode.right}' found")
+                }
+
+                currentInstruction = (currentInstruction + 1) % instructions.size
+                steps++
+            }
+
+            steps
+        }
+
+        stepsToFinalNode.forEach {
+            println("${it.key.name} -> ${it.value}")
+        }
+
+        return findLCM(stepsToFinalNode.values.toList())
     }
 
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day08_test")
-    check(part1(testInput) == 6)
+    check(part2(testInput) == 6L)
 
     val input = readInput("Day08")
     part1(input).println()
@@ -63,3 +102,25 @@ data class Node(
     val left: String,
     val right: String
 )
+
+fun findLCM(numbers: List<Long>): Long {
+    // Function to find the greatest common divisor (GCD)
+    fun findGCD(a: Long, b: Long): Long = if (b == 0L) a else findGCD(b, a % b)
+
+    // Function to find the least common multiple (LCM) of two numbers
+    fun findLCMOfTwo(a: Long, b: Long): Long = if (a == 0L || b == 0L) 0L else (a * b) / findGCD(a, b)
+
+    // Function to find the LCM of a list of numbers
+    fun findLCMOfList(numbers: List<Long>): Long {
+        if (numbers.isEmpty()) return 0L
+        var lcm = numbers[0]
+
+        for (i in 1 until numbers.size) {
+            lcm = findLCMOfTwo(lcm, numbers[i])
+        }
+
+        return lcm
+    }
+
+    return findLCMOfList(numbers)
+}
